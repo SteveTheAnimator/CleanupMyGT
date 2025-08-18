d1d8ace733678a8d06a70e4395c4c7b3c973e094
Steve Monke <103543324+SteveTheAnimator@users.noreply.github.com>
2025-08-18 05:07:08 -0600

    cleaned it up a lil bet :3

--- Plugin.cs
+++ Plugin.cs
@@ -1,53 +1,22 @@
-﻿using System;
+using System;
 using System.Runtime.InteropServices;
 using BepInEx;
 using UnityEngine;
 
 namespace CleanupMyGT
 {
-    [BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
+    [BepInPlugin("Steve.CleanupMyGT", "CleanupMyGT", "1")]
     public class Plugin : BaseUnityPlugin
     {
-        private static readonly ConsoleEventDelegate Handler = ConsoleEventCallback;
+        [DllImport("kernel32.dll")] static extern bool SetConsoleCtrlHandler(Func<int, bool> cb, bool add);
+        [DllImport("kernel32.dll")] static extern IntPtr GetCurrentProcess();
+        [DllImport("kernel32.dll")] static extern bool TerminateProcess(IntPtr h, uint c);
 
-        [DllImport("kernel32.dll")]
-        private static extern bool SetConsoleCtrlHandler(ConsoleEventDelegate callback, bool add);
-        private delegate bool ConsoleEventDelegate(int eventType);
+        void Start() => GorillaTagger.OnPlayerSpawned(() => {
+            SetConsoleCtrlHandler(e => { if (e == 2) Cleanup(); return false; }, true);
+            Application.quitting += Cleanup;
+        });
 
-        [DllImport("kernel32.dll", SetLastError = true)]
-        private static extern IntPtr GetCurrentProcess();
-
-        [DllImport("kernel32.dll")]
-        private static extern bool TerminateProcess(IntPtr hProcess, uint uExitCode);
-
-        void Awake()
-        {
-            SetConsoleCtrlHandler(Handler, true);
-            Application.quitting += OnQuit;
-        }
-
-        private void OnQuit() => Cleanup();
-
-        private static bool ConsoleEventCallback(int eventType)
-        {
-            if (eventType == 2)
-            {
-                Cleanup();
-            }
-            return false;
-        }
-
-        private static void Cleanup()
-        {
-            TerminateProcess(GetCurrentProcess(), 0);
-        }
-    }
-
-    public static class PluginInfo
-    {
-        internal const string
-            GUID = "Steve.CleanupMyGT",
-            Name = "CleanupMyGT",
-            Version = "1.0.0";
+        static void Cleanup() => TerminateProcess(GetCurrentProcess(), 0);
     }
 }
